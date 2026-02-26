3876aab8001f581f21505d2c4a1da5345e660cc3
peterfzh <peterfzh@126.com>
2026-02-27 00:34:18 +0800

    last new

--- nanobot/__main__.py
+++ nanobot/__main__.py
@@ -4,5 +4,8 @@ Entry point for running nanobot as a module: python -m nanobot
 
 from nanobot.cli.commands import app
 
+import warnings
+warnings.filterwarnings("ignore", category=RequestsDependencyWarning)
+
 if __name__ == "__main__":
     app()
